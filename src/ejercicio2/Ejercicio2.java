dde8bcd48144871938a5ac971c5e8b5c45c2fa81
chanuar santiago <y.ussef@hotmail.com>
2023-08-14 20:20:48 -0300

    hola
    hola hice un comentario

--- src/ejercicio2/Ejercicio2.java
+++ src/ejercicio2/Ejercicio2.java
@@ -15,7 +15,7 @@ public class Ejercicio2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //hola
     }
     
 }
